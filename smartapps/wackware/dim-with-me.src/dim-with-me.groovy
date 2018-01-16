a6305f456553db87a8d7ffaf8da5813fd4d75b13
Jason Charrier <jason@innovativelanguage.com>
2018-01-16 16:58:45 -0600

    added chalford's level event subscription

--- smartapps/wackware/dim-with-me.src/dim-with-me.groovy
+++ smartapps/wackware/dim-with-me.src/dim-with-me.groovy
@@ -12,7 +12,7 @@
  *		twack@wackware.net
  *  Date: 	2013-11-12
  *  Version: 	0.2
- *  
+ *
  *  Use this program with a virtual dimmer as the master for best results.
  *
  *  This app lets the user select from a list of dimmers to act as a triggering
@@ -31,11 +31,15 @@
  *		added subscription to switch.level event. Shouldn't change much
  *		but some devices only sending level event and not setLevel.
  *
+ *  Change 3:	2017-11-16 (chalford)
+ *		Added subscription to "level" event, as that is the attribute
+ *		name of the Switch Level Capability. Also reorganised subscriptions.
+ *
  ******************************************************************************
-				
+
   Other Info:	Special thanks to Danny Kleinman at ST for helping me get the
-				state stuff figured out. The Android state filtering had me 
-				stumped.
+				state stuff figured out. The Android state filtering had me
+                stumped.
  *
  ******************************************************************************
  */
@@ -43,62 +47,66 @@
 
 // Automatically generated. Make future change here.
 definition(
-	name: "Dim With Me",
-	namespace: "wackware",
-	author: "todd@wackford.net",
-	description: "Follows the dimmer level of another dimmer",
-	category: "My Apps",
-	iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-	iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience%402x.png"
+    name: "Dim With Me",
+    namespace: "wackware",
+    author: "todd@wackford.net",
+    description: "Follows the dimmer level of another dimmer",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience%402x.png"
 )
 
 preferences {
-	section("When this...") { 
-		input "masters", "capability.switchLevel", 
-			multiple: false, 
-			title: "Master Dimmer Switch...", 
+	section("When this...") {
+		input "masters", "capability.switchLevel",
+			multiple: false,
+			title: "Master Dimmer Switch...",
 			required: true
 	}
 
 	section("Then these will follow with on/off...") {
-		input "slaves2", "capability.switch", 
-			multiple: true, 
-			title: "Slave On/Off Switch(es)...", 
+		input "slaves2", "capability.switch",
+			multiple: true,
+			title: "Slave On/Off Switch(es)...",
 			required: false
 	}
-	
+
 	section("And these will follow with dimming level...") {
-		input "slaves", "capability.switchLevel", 
-			multiple: true, 
-			title: "Slave Dimmer Switch(es)...", 
+		input "slaves", "capability.switchLevel",
+			multiple: true,
+			title: "Slave Dimmer Switch(es)...",
 			required: true
 	}
 }
 
 def installed()
 {
-	subscribe(masters, "switch.on", switchOnHandler)
-	subscribe(masters, "switch.off", switchOffHandler)
-	subscribe(masters, "switch.setLevel", switchSetLevelHandler)
-	subscribe(masters, "switch", switchSetLevelHandler)
-	subscribe(slaves2, "switch.on", slavesOnHandler)
-	subscribe(slaves2, "switch.off", slavesOffHandler)
+	initializeSubscriptions()
 }
 
 def updated()
 {
 	unsubscribe()
+	initializeSubscriptions()
+}
+
+def uninstalled()
+{
+	unsubscribe()
+}
+
+def initializeSubscriptions()
+{
 	subscribe(masters, "switch.on", switchOnHandler)
 	subscribe(masters, "switch.off", switchOffHandler)
 	subscribe(masters, "switch.setLevel", switchSetLevelHandler)
-	subscribe(masters, "switch", switchSetLevelHandler)
-	subscribe(slaves2, "switch.on", slavesOnHandler)
-	subscribe(slaves2, "switch.off", slavesOffHandler)
-	log.info "subscribed to all of switches events"
+	subscribe(masters, "level", switchSetLevelHandler)
+	log.info "subscribed to switch events"
 }
 
 def switchSetLevelHandler(evt)
-{	
+{
+
 	if ((evt.value == "on") || (evt.value == "off" ))
 		return
 	def level = evt.value.toFloat()
@@ -118,30 +126,4 @@ def switchOnHandler(evt) {
 	def dimmerValue = masters.latestValue("level") //can be turned on by setting the level
 	slaves?.on()
 	slaves2?.on()
-}
-
-def slavesOnHandler(evt) {
-	def allOn = true
-	for(slave in slaves2) {
-		if(slave.currentValue("switch") == "off") {
-			allOn = false
-			last
-		}
-	}
-	if(allOn && masters.currentValue("switch") == "off") {
-		masters.on()
-	}
-}
-
-def slavesOffHandler(evt) {
-	def allOff = true
-	for(slave in slaves2) {
-		if(slave.currentValue("switch") == "on") {
-			allOff = false
-			last
-		}
-	}
-	if(allOff && masters.currentValue("switch") == "on") {
-		masters.off()
-	}
 }
